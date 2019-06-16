aae7faad395ea69e850fc6e305d9e7c0961fc60b
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-06-17 01:07:59 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/using_onchanged_to_show_input_text_flutter_05/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.using_onchanged_to_show_input_text_flutter_05;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
